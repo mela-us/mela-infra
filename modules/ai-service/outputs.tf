bb6c6c4248e857314d3afe1ae6575df955fbea2f
Tien Tran <trantien4868@gmail.com>
2025-06-14 02:07:22 +0700

    chore: refine output

--- modules/ai-service/outputs.tf
+++ modules/ai-service/outputs.tf
@@ -1,5 +1,5 @@
 output "primary_access_key" {
-  value       = azurerm_cognitive_account.openai_mela.primary_access_key
-  description = "Primary access key for the OpenAI Cognitive Account"
+  value       = azurerm_ai_services.openai_mela.primary_access_key
+  description = "Primary access key for the OpenAI AI Services"
   sensitive   = true
 }

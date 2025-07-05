2d4a3129560afcd506420a2c4ee7a9aed9f2f4ae
Tien Tran <trantien4868@gmail.com>
2025-07-05 22:06:40 +0700

    feat: add acs module

--- modules/acs/outputs.tf
+++ modules/acs/outputs.tf
@@ -1,6 +1,6 @@
 output "sender_email_address" {
   description = "The sender email address"
-  value       = azurerm_email_communication_service_domain.ecs_domain_sentry_masangroup_com.id
+  value       = azurerm_email_communication_service_domain.ecs_domain_info_mela_guru_com.id
 }
 
 output "communication_service_connection_string" {
